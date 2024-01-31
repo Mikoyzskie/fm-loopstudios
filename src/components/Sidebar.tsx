6f68a7125e5716cbc77acef202921805eaebd681
Mikoyzskie <rapidstrike13@gmail.com>
2024-01-31 15:13:41 +0800

    mobile menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,10 +10,10 @@ export default function Sidebar() {
     const { sidemenu, setSidemenu } = useCContext()
 
     return (
-        <div className={clsx("absolute bg-black inset-0 z-30 transition-all duration-200 px-[48px] pt-[78px] h-full",
-            sidemenu ? "block" : "hidden"
+        <div className={clsx("fixed bg-black md:hidden h-screen inset-x-0 z-30 transition-all duration-200 px-[48px] pt-[78px]",
+            sidemenu ? "top-0" : "-top-[1000px]"
         )}>
-            <div className=" flex justify-between items-center">
+            <div className=" flex justify-between items-center mb-[200px]">
                 <Image
                     src={"/logo.svg"}
                     width={192}
@@ -22,12 +22,12 @@ export default function Sidebar() {
                 />
                 <svg className="hover:cursor-pointer" onClick={() => setSidemenu(false)} width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fillRule="evenodd" /></svg>
             </div>
-            <ul className={`${jose.className} text-white`}>
-                <li className="uppercase">about</li>
-                <li className="uppercase">careers</li>
-                <li className="uppercase">events</li>
-                <li className="uppercase">products</li>
-                <li className="uppercase">support</li>
+            <ul className={`${jose.className} text-white flex flex-col gap-10`}>
+                <li className="uppercase text-4xl header_link w-fit">about</li>
+                <li className="uppercase text-4xl header_link w-fit">careers</li>
+                <li className="uppercase text-4xl header_link w-fit">events</li>
+                <li className="uppercase text-4xl header_link w-fit">products</li>
+                <li className="uppercase text-4xl header_link w-fit">support</li>
             </ul>
         </div>
     )
